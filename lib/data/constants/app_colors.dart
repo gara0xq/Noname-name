b80d1d810d4987b33bd46c6f46b703e39446ec04
AL-Hassan <alhassanosama88@gmail.com>
2025-12-02 16:28:14 +0200

    add image assets and update pubspec.yaml for asset inclusion

---
+++ lib/data/constants/app_colors.dart
@@ -0,0 +1,15 @@
+// lib/core/constants/app_colors.dart
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryGreen = Color(0xFF8ED8B4);
+
+  static const Color primaryOrange = Color(0xFFFAC049);
+
+  static const Color darkPrimary = Color(0xFF283442);
+  
+  static const Color taskCardYellow = Color(0xFFFE6C3B); 
+
+  static const Color beigeBackground = Color(0xFFFEF8F3);
+}
--- lib/data/constants/app_constants.dart
+++ lib/data/constants/app_constants.dart
@@ -2,7 +2,7 @@ class AppConstants {
   static const String BASE_URL = 'https://ibmintern2025.vercel.app';
 
   static const String MOCK_AUTH_TOKEN = //Testing with real token until finishing the Authorization process
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2OTJkZTY0YTkwMDkzMDEzZjcwZWY0OTciLCJmYW1pbHlJZCI6IjY5MmRlNjRhOTAwOTMwMTNmNzBlZjQ5NCIsInBhcmVudElkIjoiNjkyZGU2NGE5MDA5MzAxM2Y3MGVmNDk5Iiwicm9sZSI6InBhcmVudCIsImlhdCI6MTc2NDYxNTgyNn0.HlU9Znal73dQtr5NJBAJO2XR_hOk9uVqND_bb_18-84';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2OTJkZTY0YTkwMDkzMDEzZjcwZWY0OTciLCJmYW1pbHlJZCI6IjY5MmRlNjRhOTAwOTMwMTNmNzBlZjQ5NCIsInBhcmVudElkIjoiNjkyZGU2NGE5MDA5MzAxM2Y3MGVmNDk5Iiwicm9sZSI6InBhcmVudCIsImlhdCI6MTc2NDY3NzAyOH0.FWs7YqqjDwTM4TfE1yZNSEcuAIR6mppDNxSDfLlEA78';
 
 
   static const String AUTH_HEADER_KEY = 'Authorization';
